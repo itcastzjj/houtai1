d96355b58d0dff496d79c4a6bf7da4f7daf7dc1a
Arthur <2475079253@qq.com>
2019-04-13 17:23:53 +0800

    实现屏幕自适应

---
+++ src/views/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="login">登录</div>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  },
+  methods: {
+    myfun () {
+      var a = 'adsd45655'
+      var b = /^[a-z_][0-9]{1,15}$/
+      var c = /^[a-zA-Z][a-zA-Z0-9_]{4,15}$/
+      if (c.test(a)) {
+        console.log(1)
+      } else {
+        console.log(2)
+      }
+    }
+  },
+  mounted () {
+    this.myfun()
+  }
+}
+</script>
+<style lang="scss" scoped>
+.login {
+  background-color: #f00;
+  width: 1rem;
+  height: 1rem;
+  font-size: 0.18rem;
+}
+</style>
